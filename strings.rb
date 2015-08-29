f3f2cadf68993d25fbe0190c18b1918466c2756e
shegl <mihastifler@gmail.com>
2015-08-29 12:31:38 +0300

    Some work with strings at ruby

--- strings.rb
+++ strings.rb
@@ -5,4 +5,8 @@
 first = 'Mary had'
 second = " a little lamb"
 poem = first + second
-puts poem
+puts "The result string is #{poem}"
+
+puts "The lenght of 'first' is #{first.length}"
+puts poem.upcase
+puts poem.downcase
