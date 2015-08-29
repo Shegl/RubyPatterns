cf30a978045b4791c16dff88bc0fb66dc8c05176
shegl <mihastifler@gmail.com>
2015-08-29 14:38:34 +0300

    Hashes are Done

---
+++ hashes.rb
@@ -0,0 +1,10 @@
+# hashes
+
+h = {}
+h['first_name'] = 'Albert'
+h['last_name'] = 'Einstein'
+
+h = {'first_name' => 'Albert', 'last_name' => 'Einstein'} #
+h = {:first_name => 'Albert', :last_name => 'Einstein'} # There is no new 'bout
+
+puts "#{h[:first_name]} #{h[:last_name]}"
